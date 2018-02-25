68d3bdefebebe64e86c6ae6766cbb4192a0aa49d
siergirj <tdzierzano@gmail.com>
2018-02-25 16:07:40 +0100

    Eve for province

---
+++ settings.py
@@ -0,0 +1,52 @@
+# mongo DB
+MONGO_DBNAME = 'teryt'
+
+schema = {
+    # Schema definition, based on Cerberus grammar. Check the Cerberus project
+    # (https://github.com/pyeve/cerberus) for details.
+    'firstname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 10,
+    },
+    'lastname': {
+        'type': 'string',
+        'minlength': 1,
+        'maxlength': 15,
+        'required': True,
+        # talk about hard constraints! For the purpose of the demo
+        # 'lastname' is an API entry-point, so we need it to be unique.
+        'unique': True,
+    },
+    # 'role' is a list, and can only contain values from 'allowed'.
+    'role': {
+        'type': 'list',
+        'allowed': ["author", "contributor", "copy"],
+    },
+    # An embedded 'strongly-typed' dictionary.
+    'location': {
+        'type': 'dict',
+        'schema': {
+            'address': {'type': 'string'},
+            'city': {'type': 'string'}
+        },
+    },
+    'born': {
+        'type': 'datetime',
+    },
+}
+
+province = {
+    # 'title' tag used in item links. Defaults to the resource title minus
+    # the final, plural 's' (works fine in most cases but not for 'people')
+    'item_title': 'person',
+
+    # support only GET
+    'resource_methods': ['GET'],
+
+    'schema': schema
+}
+
+DOMAIN = {
+    'province': province,
+}
